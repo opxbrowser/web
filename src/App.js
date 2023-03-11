fe669fba5ea79058ea69ffb3349a3a3e84c61a7e
Hubert Ryan <hubert.ryan@nickelpay.com.br>
2023-03-11 16:38:44 -0300

    react config added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function App() {
-  return <div>App</div>;
+  return <div></div>;
 }
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    ...colors,
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#8A4FFF",
+          200: "#F7F6FF",
+          400: "#C3BEF7",
+        },
+        secundary: {
+          DEFAULT: "#FB4FFF",
+        },
+        dark: {
+          DEFAULT: "#070600",
+          400: "#B5B4C2",
+          500: "#505050",
+        },
+        gray: {
+          DEFAULT: "#F5F5F5",
+        },
+      },
+    },
+    fontFamily: {
+      sans: ["Work Sans", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
