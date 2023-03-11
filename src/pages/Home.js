e2546fabf1da80323944edd1664d99174ef7c3e2
Arkadina <61208486+Arkadina@users.noreply.github.com>
2023-03-11 17:38:41 -0300

    header component created and react router dom added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Header from "../components/Header";
 
 export default function Home() {
-  return <div>Home</div>;
+    return (
+        <div>
+            <Header />
+        </div>
+    );
 }
