83e81199474c1548b23e212f5f720a6361602e95
Arkadina <61208486+Arkadina@users.noreply.github.com>
2023-03-11 17:47:47 -0300

    Fixed home main div width

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 
 export default function Home() {
     return (
-        <div>
+        <div className="w-full ">
             <Header />
         </div>
     );
